a94ce83953af5d38c9270590ca6b9a8ba66c4f01
fierrito2015 <47386049+fierrito2015@users.noreply.github.com>
2019-08-20 21:49:54 -0300

    Add files via upload

---
+++ Ejercicio_02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 02";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            int num;
+            double cuadrado;
+            double cubo;
+
+            Console.Write("Ingrese un Numero Mayor a 0: ");
+            num = int.Parse(Console.ReadLine());
+             while (num <= 0)
+            {
+                Console.Write("EROOR!! Reingrese un Numero mayor a 0: ");
+                num = int.Parse(Console.ReadLine());
+
+            }
+
+            cuadrado =  Math.Pow(num,2);
+            cubo = Math.Pow(num, 3);
+
+            Console.WriteLine("El cuadrado de {0} es {1}", num, cuadrado);
+            Console.WriteLine("El cubo de {0} es {1}", num, cubo);
+            Console.ReadLine();
+            
+                
+
+            
+
+
+        }
+    }
+}
---
+++ Ejercicio_03/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 03";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            int num;
+            int i;
+            int j;
+            int contador = 0;
+
+            Console.Write("Ingrese un numero mayor a 0: ");
+            num = int.Parse( Console.ReadLine());
+
+            while (num <= 0)
+            {
+                Console.Write("EROOR!! Reingrese un Numero mayor a 0: ");
+                num = int.Parse(Console.ReadLine());
+
+            }
+
+            for(i=0;i<=num;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    if(i>0 && i%j==0)
+                    {
+                        contador++;
+                    }
+                }
+                if(contador==2)
+                {
+                    Console.WriteLine("{0}", i);
+                    contador = 0;
+                }
+
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
