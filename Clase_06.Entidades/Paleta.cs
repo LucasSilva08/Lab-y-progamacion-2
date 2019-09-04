dec953399bfd4c793a9c06470f99973af66f0f54
lucas <lucasnicolas.silva@hotmail.com>
2019-09-04 11:59:30 -0300

    Clase_06

---
+++ Clase_06.Entidades/Paleta.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_06.Entidades
+{
+    public class Paleta
+    {
+        private Tempera[] colores;
+        private int cantidadMaximaColores;
+
+        private Paleta():this(5)
+        {
+            //this.cantidadMaximaColores = 5;
+            
+            //this.colores = new Tempera[this.cantidadMaximaColores];
+                       
+        }
+        private Paleta(int CantidadElementos)
+        {
+            this.colores = new Tempera[CantidadElementos];
+            this.cantidadMaximaColores = CantidadElementos;
+            
+        }
+        public static implicit operator Paleta(int cantidad)
+        {
+            Paleta Pal;
+            Pal = new Paleta(cantidad);
+            return Pal;
+            
+        }
+        private string Mostrar ()
+        {
+            string resultado = "";
+            for(int i=0;i<this.colores.Length;i++)
+            {
+                resultado += this.colores[i];
+            }
+
+
+            return resultado+"-Cantidad: "+ this.cantidadMaximaColores; 
+        }
+        public static explicit operator string (Paleta Pal)
+        {
+            return Pal.Mostrar();
+
+        }
+        public static bool operator == (Paleta Pal, Tempera temp)
+        {
+            bool retorno = false;
+            int largo = Pal.cantidadMaximaColores;
+            if( !Object.Equals(Pal,null) && !Object.Equals(temp,null))
+            {
+
+                for (int i = 0; i < largo; i++)
+                {
+                    if (Pal.colores[i] == temp)
+                    {
+                        retorno = true;
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+                retorno = Object.Equals(Pal, temp);
+            }
+            
+            return retorno;
+
+        }
+        public static bool operator !=(Paleta Pal, Tempera temp)
+        {
+            return !(Pal == temp);
+        }
+        private int ObtenerLugarLibre()
+        {
+            int lugarLibre=-1;
+            int i;
+            for (i=0;i<this.cantidadMaximaColores;i++)
+            {
+                if(this.colores[i]==null)
+                {
+                    lugarLibre = i;
+                    break;
+                }
+            }
+            return lugarLibre; 
+        }
+        private int ObtenerLugarDeTempera(Tempera temp)
+        {
+            int indice=-1;
+            int i;
+            for(i=0;i<this.cantidadMaximaColores;i++)
+            {
+                if(this.colores[i]==temp)
+                {
+                    indice = i;
+                    break;
+                }
+            }
+            return indice;
+
+        }
+        public static Paleta operator +(Paleta pal,Tempera temp)
+        {
+            int index;
+            if(!(pal==temp))
+            {
+                index = pal.ObtenerLugarLibre();
+                if(index!=-1)
+                {
+                    pal.colores[index] = temp;
+                }
+            }
+            else
+            {
+                index = pal.ObtenerLugarDeTempera(temp);
+                if(index!=-1)
+                {
+                    pal.colores[index] += temp;
+                }
+            }
+            return pal;
+        }
+        public static int operator |(Paleta pal, Tempera temp)
+        {
+
+        }
+
+    }
+}
---
+++ Clase_06.Entidades/Tempera.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Clase_06.Entidades
+{
+    public class Tempera
+    {
+        private ConsoleColor color;
+        private string marca;
+        private int cantidad;
+
+        public Tempera (ConsoleColor c, string m,int cant)
+        {
+            this.color = c;
+            this.marca = m;
+            this.cantidad = cant;
+        }
+        private string Mostrar ()
+        {
+            return "Marca: " + this.marca + "- Color: " + this.color + "-Cantidad: " + this.cantidad;
+        }
+        public static implicit operator string (Tempera temp)
+        {
+            return temp.Mostrar();
+        }
+        public static bool operator == (Tempera tem1, Tempera tem2)
+        {
+            bool retorno = false;
+            if (!tem1.Equals(null) && !tem2.Equals(null))
+            {
+                if (tem1.color == tem2.color && tem1.marca == tem2.marca)
+                {
+                    retorno = true;
+
+                }
+            }
+            return retorno;
+        }
+        public static bool operator !=(Tempera tem1, Tempera tem2)
+        {
+            
+            return !(tem1 == tem2);
+        }
+        public static Tempera operator +(Tempera temp,int cant)
+        {
+            if(!temp.Equals(null))
+            {
+              temp.cantidad = temp.cantidad + cant;
+              
+            }
+            return temp;
+        }
+        public static Tempera operator +(Tempera temp1,Tempera temp2)
+        {
+            if(temp1==temp2)
+            {
+                temp1 = temp1 + temp2.cantidad;
+            }
+            return temp1;
+        }
+            
+
+    }
+}
