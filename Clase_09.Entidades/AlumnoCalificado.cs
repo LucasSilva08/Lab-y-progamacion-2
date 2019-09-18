549be8635d0281333fe63ba23208fa3d914354dd
alumno <alumno@utnfra.com.ar>
2019-09-18 12:00:21 -0300

    HERENCIAS

---
+++ Clase_09.Entidades/AlumnoCalificado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_09.Entidades
+{
+    public class AlumnoCalificado : Alumno
+    {
+        protected double nota;
+
+        //PROPIEDADES//
+        public double Nota { get {return this.nota; } }
+
+        //CONSTRUCTORES//
+        public AlumnoCalificado(Alumno a, double nota):this(a.Nombre,a.Apellido,a.Legajo,a.Examen,nota)
+        {
+
+        }
+        public AlumnoCalificado(string nombre,string apellido,int legajo,ETipoExamen examen,double nota) : base(nombre,apellido,legajo,examen)
+        {
+            this.nota = nota;
+
+        }
+        //METODO//
+        public string Mostrar()
+        {
+            return Alumno.Mostrar(this) + " Nota: " + this.nota;
+        }
+    }
+}
